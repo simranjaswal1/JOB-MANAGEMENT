4dda8e1827bf10b9aabc7513219c73744dfdb75c
simranjaswal1 <123365833+simranjaswal1@users.noreply.github.com>
2024-09-02 09:32:37 +0530

    update navbar.jsx

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
                         !user ? (
                             <div className='flex items-center gap-2'>
                                 <Link to="/login"><Button variant="outline">Login</Button></Link>
-                                <Link to="/signup"><Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">Signup</Button></Link>
+                                <Link to="/signup"><Button variant="outline">Signup</Button></Link>
                             </div>
                         ) : (
                             <Popover>
