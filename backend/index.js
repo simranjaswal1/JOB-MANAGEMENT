82322b4f3436e3044a5ace1fda83623ee17c771c
simranjaswal1 <123365833+simranjaswal1@users.noreply.github.com>
2025-04-23 15:25:51 +0530

    CORS

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://your-frontend-site.netlify.app'],
     credentials:true
 }
 
