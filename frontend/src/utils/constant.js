279905cf460348aa15760b7b809ed0aabea864e5
simranjaswal1 <123365833+simranjaswal1@users.noreply.github.com>
2025-04-23 15:35:59 +0530

    Update constant.js
    .

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,10 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+const isLocalhost = window.location.hostname === 'localhost';
+
+const BASE_API_URL = isLocalhost
+  ? 'http://localhost:8000/api/v1'
+  : 'https://job-management-1yq7.onrender.com/api/v1';
+
+export const USER_API_END_POINT = `${BASE_API_URL}/user`;
+export const JOB_API_END_POINT = `${BASE_API_URL}/job`;
+export const APPLICATION_API_END_POINT = `${BASE_API_URL}/application`;
+export const COMPANY_API_END_POINT = `${BASE_API_URL}/company`;
